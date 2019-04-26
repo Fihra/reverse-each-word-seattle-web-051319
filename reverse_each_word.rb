672601188892b940c7c643a19f06fb4a91f14034
Fabian Fabro <fabrofabian@gmail.com>
2019-04-26 02:45:41 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,6 +2,6 @@ def reverse_each_word(string)
   split_string = string.split(" ")
   reversed = []
 
-
-  split_string.each
+  split_string.each do |word|
+    word.size.times {reversed << split_string.pop}
 end
